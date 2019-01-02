158475390ea13a5d7a091fb09fcde9b95bf7894d
Raffy <raffy_panes@hotmail.com>
2019-01-02 11:38:14 +0800

    Comments Stuff Routes

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,10 +1,11 @@
+// This is the ROUTE Module
 const express = require('express');
 const router = express.Router();
-
+/* AUTHENTICATION middleware */
 const auth = require('../middleware/auth');
-
+/* CONTROLLER */
 const stuffCtrl = require('../controllers/stuff');
-
+/* ROUTE Declrations */
 router.get('/', auth, stuffCtrl.getAllStuff);
 router.post('/', auth, stuffCtrl.createThing);
 router.get('/:id', auth, stuffCtrl.getOneThing);
